6659b279b1557242704042f2d8f0e93cba8e40b5
Valentin Krasontovitsch <v.krasontov@gmail.com>
2018-01-13 23:30:13 +0100

    Feat: Start vue project, write first dummy component

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <Connection/>
+  </div>
+</template>
+
+<script>
+import Connection from './components/Connection'
+
+export default {
+  name: 'App',
+  components: {
+    Connection
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ src/components/Connection.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="connection-status">
+    <h1>{{ connectionStatus }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Connection',
+  data () {
+    return {
+      connectionStatus: false
+    }
+  }
+}
+</script>
+
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+</style>
